a35a448000bb8831872a0c45991151a77d76b668
gabiAlmeidaDev <153233088+gabiAlmeidaDev@users.noreply.github.com>
2024-03-25 09:38:09 -0300

    commit inicial

---
+++ src/test/java/com/tutoria/semana10/Semana10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutoria.semana10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semana10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
